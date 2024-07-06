0c5b79c48e56cf89cb27a722af783f49b821cc54
anubhav2525 <anbhavranjan2525@gmail.com>
2024-07-07 00:52:21 +0530

    complete mobile app view

--- src/components/MobileApp/MobileApp.jsx
+++ src/components/MobileApp/MobileApp.jsx
@@ -3,33 +3,36 @@ import React from 'react'
 const MobileApp = () => {
     const data = [{
         id: 1,
-        appName: "AppName",
-        downloadApp: "App",
-        appDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus!",
-        appLink: "www.google.com",
-        userManual: "",
-        videoDemo: "",
-        suggestion: ""
+        appName: "Informer App",
+        downloadApp: "Informer App",
+        appDescription: "राज्य सरकार द्वारा संचालित इनफोरमर (मुखबिर) योजना के तहत राजस्व हानि में लिप्त व्यक्तियों, कम्पनी एवं फर्म के बारे में सत्य सूचना इनफोरमर ऐप के माध्यम से सम्बन्धित विभाग को भेजी जा सकती है।",
+        appLink: "",
+        userManual: "https://excise.rajasthan.gov.in/MOBILEAPI/informerlatestusermanual.pdf",
+        videoDemo: "https://excise.rajasthan.gov.in/MOBILEAPI/informerlatestusermanual.pdf",
+        suggestion: 0,
+        isNew: true
     },
     {
         id: 2,
-        appName: "",
-        downloadApp: "",
-        appDescription: "Lorem consectetur adipisicing elit. Natus!",
+        appName: "Raj Excise Citizen",
+        downloadApp: "Raj Excise Citizen",
+        appDescription: "राजस्थान के शराब की बोतल कोड का वास्तविक विवरण प्राप्त करने के लिए बोतल कोड नंबर दर्ज करें या स्कैन करें।",
         appLink: "",
-        userManual: "",
-        videoDemo: "",
-        suggestion: ""
+        userManual: " https://excise.rajasthan.gov.in/MOBILEAPI/Excisecitizenusermanual.pdf",
+        videoDemo: "https://excise.rajasthan.gov.in/MOBILEAPI/Excisecitizenusermanual.pdf",
+        suggestion: 0,
+        isNew: true
     },
     {
         id: 3,
-        appName: "",
-        downloadApp: "",
-        appDescription: "Lorem ipsum dolor sit amet consectetur adipisic",
+        appName: "गन्ना किसान के लिए मोबाइल एप्लिकेशन",
+        downloadApp: "गन्ना किसान के लिए मोबाइल एप्लिकेशन",
+        appDescription: "किसान को प्रत्येक सूचना समय-समय पर देखने , अपनी एंट्री स्वयं करने एवं गन्ने के एवज में प्राप्त होने वाली राशि आदि देखने का अधिकार |",
         appLink: "",
-        userManual: "",
-        videoDemo: "",
-        suggestion: "22"
+        userManual: "https://excise.rajasthan.gov.in/MOBILEAPI/FarmerApp.pdf",
+        videoDemo: "https://excise.rajasthan.gov.in/MOBILEAPI/CaneFarmerAppPPTFinal.mp4",
+        suggestion: 22,
+        isNew: false
     }]
 
     return (
@@ -59,35 +62,35 @@ const MobileApp = () => {
             <h2 className="mb-4 text-2xl font-extrabold leading-none text-gray-600 md:text-3xl dark:text-slate-300 tracking-wide">Mobile Application</h2>
 
             {/* table  */}
-            <div className='my-4 bg-slate-300 dark:bg-slate-800 px-2 rounded sm:px-4 md:px-8 py-2'>
+            <div className='my-4 bg-slate-300 dark:bg-slate-800 px-2 rounded sm:px-4 py-2'>
                 {/* table here  */}
                 <div className="relative rounded overflow-x-auto shadow-md sm:rounded-lg">
                     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-600 dark:text-gray-300">
-                    <thead className="text-xs text-white uppercase bg-gray-400 dark:bg-gray-400 dark:text-white">
+                        <table className="w-full text-sm text-left rtl:text-right text-gray-600 dark:text-gray-300">
+                            <thead className="text-xs text-white uppercase bg-gray-400 dark:bg-gray-400 dark:text-white">
                                 <tr>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-4 py-3 border-r border-white">
                                         S. No.
                                     </th>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-2 py-3 border-r border-white">
                                         APP Name
                                     </th>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-2 py-3 border-r border-white">
                                         Download APP
                                     </th>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-2 py-3 border-r border-white">
                                         APP Description
                                     </th>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-2 py-3 border-r border-white">
                                         Download APP
                                     </th>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-2 py-3 border-r border-white">
                                         APP User Manual
                                     </th>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-2 py-3 border-r border-white">
                                         APP Video
                                     </th>
-                                    <th scope="col" className="px-6 py-3">
+                                    <th scope="col" className="px-2 py-3 border-r border-white">
                                         Suggestion
                                     </th>
                                 </tr>
@@ -96,21 +99,26 @@ const MobileApp = () => {
                                 {
                                     data.map((items, index) => {
                                         return (
-                                            <tr key={index} className="bg-white tracking-wide border-b dark:bg-gray-600 dark:border-gray-700">
-                                                <td className="px-6 py-4">
+                                            <tr key={index} className="bg-white border-b dark:bg-gray-600 dark:border-gray-700">
+                                                <td className="px-6 py-2 border-x border-slate-300">
                                                     {items.id}
                                                 </td>
-                                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                    {items.appName}
+                                                <th scope="row" className="px-6 py-2 font-medium text-gray-900 border-x border-slate-300 dark:text-white flex flex-col gap-3 ">
+                                                    <div>
+                                                        {items.appName}
+                                                    </div>
+                                                    <div>
+                                                        {items.isNew && <span className="bg-green-500 text-green-100 text-xs font-medium px-3 py-0.5 rounded-md dark:bg-green-900 dark:text-green-300">New</span>}
+                                                    </div>
                                                 </th>
-                                                <td className="px-6 py-4">
+                                                <td className="px-2 py-2 border-x border-slate-300">
                                                     {items.downloadApp}
                                                 </td>
-                                                <td className="px-6 py-4">
+                                                <td className="px-2 py-2 border-x border-slate-300">
                                                     {items.appDescription}
                                                 </td>
-                                                <td className="px-6 py-4">
-                                                    <a href={items.appLink} className=''>
+                                                <td className="px-2 py-2 border-x border-slate-300">
+                                                    <a href={items.appLink} target="_blank" className='flex justify-center items-center'>
                                                         <svg className="w-6 h-6 text-gray-600 dark:text-white dark:hover:text-slate-400 hover:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                                             <path fillRule="evenodd" d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z" clipRule="evenodd" />
                                                             <path fillRule="evenodd" d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clipRule="evenodd" />
@@ -118,23 +126,23 @@ const MobileApp = () => {
 
                                                     </a>
                                                 </td>
-                                                <td className="px-6 py-4">
-                                                    <a href={items.userManual}>
+                                                <td className="px-2 py-2 border-x border-slate-300">
+                                                    <a href={items.userManual} target="_blank" className='flex justify-center items-center'>
                                                         <svg className="w-6 h-6 text-gray-600 dark:text-white dark:hover:text-slate-400 hover:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                                             <path fillRule="evenodd" d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z" clipRule="evenodd" />
                                                             <path fillRule="evenodd" d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clipRule="evenodd" />
                                                         </svg>
                                                     </a>
                                                 </td>
-                                                <td className="px-6 py-4">
-                                                    <a href={items.videoDemo}>
+                                                <td className="px-2 py-2 border-x border-slate-300">
+                                                    <a href={items.videoDemo} target="_blank" className='w-full flex justify-center items-center'>
                                                         <svg className="w-6 h-6 text-gray-600 dark:text-white dark:hover:text-slate-400 hover:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                                             <path fillRule="evenodd" d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z" clipRule="evenodd" />
                                                             <path fillRule="evenodd" d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clipRule="evenodd" />
                                                         </svg>
                                                     </a>
                                                 </td>
-                                                <td className="px-6 py-4">
+                                                <td className="px-2 py-2 flex justify-center items-center border-x border-slate-300">
                                                     {items.suggestion}
                                                 </td>
                                             </tr>
